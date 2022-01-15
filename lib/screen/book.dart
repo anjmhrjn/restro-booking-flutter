2a7eef570a05f1ab75ae2c80e3de1b2a6472d0ec
anjmhrjn <anjmhrjn1@gmail.com>
2022-01-15 23:53:02 +0545

    Code optimization of booking table screen

--- lib/screen/book.dart
+++ lib/screen/book.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restro_booking/screen/getTablesForm.dart';
 import 'package:restro_booking/screen/info.dart';
 import 'package:date_field/date_field.dart';
 
@@ -32,13 +33,73 @@ class _BookTableState extends State<BookTable> {
       detail: '1-8',
     ),
   ];
-  @override
-  Widget build(BuildContext context) {
+
+  Widget getRestroInfo(BuildContext context) {
     var size = MediaQuery.of(context).size;
 
-    /*24 is for notification bar on Android*/
     final double itemHeight = (size.height - kToolbarHeight - 24) / 10;
     final double itemWidth = size.width / 2;
+
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(0xFFFFFFFF),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: GridView.count(
+          // controller:
+          //     new ScrollController(keepScrollOffset: false),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          childAspectRatio: (itemWidth / itemHeight),
+          crossAxisCount: 2,
+          crossAxisSpacing: 15.0,
+          mainAxisSpacing: 10.0,
+          children: List.generate(restroInfo.length, (index) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      restroInfo[index].icon,
+                      color: Color(0xFFACC9DC),
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      restroInfo[index].title!,
+                      style: TextStyle(
+                        color: Color(0xFFACC9DC),
+                        fontWeight: FontWeight.w800,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  restroInfo[index].detail!,
+                  style: TextStyle(
+                    color: Color(0xFF041838),
+                  ),
+                ),
+              ],
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -74,62 +135,7 @@ class _BookTableState extends State<BookTable> {
                       SizedBox(
                         height: 20,
                       ),
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFFFFFFF),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: GridView.count(
-                            // controller:
-                            //     new ScrollController(keepScrollOffset: false),
-                            shrinkWrap: true,
-                            scrollDirection: Axis.vertical,
-                            childAspectRatio: (itemWidth / itemHeight),
-                            crossAxisCount: 2,
-                            crossAxisSpacing: 15.0,
-                            mainAxisSpacing: 10.0,
-                            children: List.generate(restroInfo.length, (index) {
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        restroInfo[index].icon,
-                                        color: Color(0xFFACC9DC),
-                                        size: 20,
-                                      ),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      Text(
-                                        restroInfo[index].title!,
-                                        style: TextStyle(
-                                          color: Color(0xFFACC9DC),
-                                          fontWeight: FontWeight.w800,
-                                          fontSize: 15,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text(
-                                    restroInfo[index].detail!,
-                                    style: TextStyle(
-                                      color: Color(0xFF041838),
-                                    ),
-                                  ),
-                                ],
-                              );
-                            }),
-                          ),
-                        ),
-                      ),
+                      getRestroInfo(context),
                     ],
                   ),
                 ),
@@ -139,193 +145,7 @@ class _BookTableState extends State<BookTable> {
                   horizontal: 20,
                   vertical: 30,
                 ),
-                child: Form(
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 17,
-                          ),
-                          hintText: 'Enter number of seats',
-                          labelText: 'No. of seats',
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            borderSide: BorderSide(
-                              color: Color(0xFFACC9DC),
-                              width: 2.0,
-                            ),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Color(0xFFACC9DC),
-                              width: 2.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      DateTimeFormField(
-                        mode: DateTimeFieldPickerMode.date,
-                        decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 17,
-                          ),
-                          errorStyle: TextStyle(color: Colors.redAccent),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Color(0xFFACC9DC),
-                              width: 2.0,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Color(0xFFACC9DC),
-                              width: 2.0,
-                            ),
-                          ),
-                          suffixIcon: Icon(Icons.event_note),
-                          labelText: 'Date',
-                          hintText: 'Select date for reservation',
-                        ),
-                        firstDate: DateTime.now(),
-                        initialDate: DateTime.now().add(
-                          const Duration(days: 10),
-                        ),
-                        autovalidateMode: AutovalidateMode.always,
-                        validator: (DateTime? e) => (e?.day ?? 0) == 1
-                            ? 'Please not the first day'
-                            : null,
-                        onDateSelected: (DateTime value) {
-                          print(value);
-                        },
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: (size.width - 80) / 2,
-                            child: DateTimeFormField(
-                              mode: DateTimeFieldPickerMode.time,
-                              decoration: const InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 20,
-                                  vertical: 17,
-                                ),
-                                errorStyle: TextStyle(color: Colors.redAccent),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10.0),
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Color(0xFFACC9DC),
-                                    width: 2.0,
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10.0),
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Color(0xFFACC9DC),
-                                    width: 2.0,
-                                  ),
-                                ),
-                                labelText: 'Time',
-                                hintText: 'Start Time',
-                              ),
-                              firstDate: DateTime.now(),
-                              initialDate: DateTime.now().add(
-                                const Duration(days: 10),
-                              ),
-                              autovalidateMode: AutovalidateMode.always,
-                              validator: (DateTime? e) {
-                                return (e?.day ?? 0) == 1
-                                    ? 'Please not the first day'
-                                    : null;
-                              },
-                              onDateSelected: (DateTime value) {
-                                print(value);
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            '-',
-                            textScaleFactor: 2,
-                          ),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Container(
-                            width: (size.width - 80) / 2,
-                            child: DateTimeFormField(
-                              mode: DateTimeFieldPickerMode.time,
-                              decoration: const InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 20,
-                                  vertical: 17,
-                                ),
-                                errorStyle: TextStyle(color: Colors.redAccent),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10.0),
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Color(0xFFACC9DC),
-                                    width: 2.0,
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10.0),
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Color(0xFFACC9DC),
-                                    width: 2.0,
-                                  ),
-                                ),
-                                labelText: 'Time',
-                                hintText: 'End Time',
-                              ),
-                              firstDate: DateTime.now(),
-                              initialDate: DateTime.now().add(
-                                const Duration(days: 10),
-                              ),
-                              autovalidateMode: AutovalidateMode.always,
-                              validator: (DateTime? e) {
-                                return (e?.day ?? 0) == 1
-                                    ? 'Please not the first day'
-                                    : null;
-                              },
-                              onDateSelected: (DateTime value) {
-                                print(value);
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+                child: getTablesForm(context),
               ),
               Padding(
                 padding: const EdgeInsets.only(
---
+++ lib/screen/getTablesForm.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:date_field/date_field.dart';
+
+Widget getTablesForm(BuildContext context) {
+  var size = MediaQuery.of(context).size;
+  return Form(
+    child: Column(
+      children: [
+        TextFormField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 17,
+            ),
+            hintText: 'Enter number of seats',
+            labelText: 'No. of seats',
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Color(0xFFACC9DC),
+                width: 2.0,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              borderSide: BorderSide(
+                color: Color(0xFFACC9DC),
+                width: 2.0,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        DateTimeFormField(
+          mode: DateTimeFieldPickerMode.date,
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 17,
+            ),
+            errorStyle: TextStyle(color: Colors.redAccent),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              borderSide: BorderSide(
+                color: Color(0xFFACC9DC),
+                width: 2.0,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              borderSide: BorderSide(
+                color: Color(0xFFACC9DC),
+                width: 2.0,
+              ),
+            ),
+            suffixIcon: Icon(Icons.event_note),
+            labelText: 'Date',
+            hintText: 'Select date for reservation',
+          ),
+          firstDate: DateTime.now(),
+          initialDate: DateTime.now().add(
+            const Duration(days: 10),
+          ),
+          autovalidateMode: AutovalidateMode.always,
+          validator: (DateTime? e) =>
+              (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+          onDateSelected: (DateTime value) {
+            print(value);
+          },
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          children: [
+            Container(
+              width: (size.width - 80) / 2,
+              child: DateTimeFormField(
+                mode: DateTimeFieldPickerMode.time,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 17,
+                  ),
+                  errorStyle: TextStyle(color: Colors.redAccent),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color(0xFFACC9DC),
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color(0xFFACC9DC),
+                      width: 2.0,
+                    ),
+                  ),
+                  labelText: 'Start Time',
+                  hintText: 'Start Time',
+                ),
+                firstDate: DateTime.now(),
+                initialDate: DateTime.now().add(
+                  const Duration(days: 10),
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: (DateTime? e) {
+                  return (e?.day ?? 0) == 1 ? 'Please not the first day' : null;
+                },
+                onDateSelected: (DateTime value) {
+                  print(value);
+                },
+              ),
+            ),
+            SizedBox(
+              width: 13,
+            ),
+            Text(
+              'to',
+              // textScaleFactor: 2,
+            ),
+            SizedBox(
+              width: 13,
+            ),
+            Container(
+              width: (size.width - 80) / 2,
+              child: DateTimeFormField(
+                mode: DateTimeFieldPickerMode.time,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 17,
+                  ),
+                  errorStyle: TextStyle(color: Colors.redAccent),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color(0xFFACC9DC),
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color(0xFFACC9DC),
+                      width: 2.0,
+                    ),
+                  ),
+                  labelText: 'End Time',
+                  hintText: 'End Time',
+                ),
+                firstDate: DateTime.now(),
+                initialDate: DateTime.now().add(
+                  const Duration(days: 10),
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: (DateTime? e) {
+                  return (e?.day ?? 0) == 1 ? 'Please not the first day' : null;
+                },
+                onDateSelected: (DateTime value) {
+                  print(value);
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
