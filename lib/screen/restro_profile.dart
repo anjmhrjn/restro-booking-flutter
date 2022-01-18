960f566b276568af1fec628611c217c648aaff53
anjmhrjn <anjmhrjn1@gmail.com>
2022-01-18 20:59:51 +0545

    Restaurant Details page complete

--- lib/screen/restro_profile.dart
+++ lib/screen/restro_profile.dart
@@ -1,38 +1,244 @@
 import 'package:flutter/material.dart';
+import 'package:bordered_text/bordered_text.dart';
 
 class RestroProfile extends StatelessWidget {
   const RestroProfile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const List<String> tag_list = ["Cafe", "Indian", "Spicy Foods"];
+    const List<String> image_list = [
+      "images/snacks.jpg",
+      "images/restro1.jpg",
+      "images/aloo nazakat.jpg"
+    ];
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  // image: DecorationImage(
-                  //   image: AssetImage(
-                  //     'images/restro.png',
-                  //   ),
-                  //   fit: BoxFit.fill,
-                  // ),
-                  color: Color(0xFFF6F2EC),
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.zero,
-                    bottom: Radius.circular(35),
+              Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 240,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'images/aloo nazakat.jpg',
+                        ),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(1),
+                          BlendMode.dstATop,
+                        ),
+                      ),
+                      color: Color(0xFFF6F2EC),
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.zero,
+                        bottom: Radius.circular(35),
+                      ),
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: SizedBox(
+                      height: 220,
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: BorderedText(
+                          strokeWidth: 5.0,
+                          child: Text(
+                            'Lorem Ipsum Cafe',
+                            style: TextStyle(
+                              color: Color(0xFFF6F2EC),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 27,
+                              letterSpacing: 1.5,
+                              // decorationColor: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
                 ),
-                child: Image.asset(
-                  'images/test.png',
-                  width: 600.0,
-                  height: 240.0,
-                  fit: BoxFit.fill,
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.location_on,
+                          color: Color(0xFFACC9DC),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Jhamsikhel, Lalitpur',
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            color: Color(0xFF041838),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      height: 30,
+                      child: ListView.builder(
+                        itemCount: tag_list.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xFF004194),
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 15,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                tag_list[index],
+                                style: TextStyle(
+                                  color: Color(0xFFF6F2EC),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'About Us',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                      style: TextStyle(height: 1.4),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Popular Items',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 150,
+                      child: ListView.builder(
+                        itemCount: tag_list.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 10, right: 10),
+                            child: SizedBox(
+                              width: 150,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                // height: 100,
+                                child: Image.asset(
+                                  image_list[index],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFACC9DC),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'See Menu',
+                            style: TextStyle(
+                              color: Color(0xFF041838),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            size: 16,
+                            color: Color(0xFF041838),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFEE6A3E),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Book Now',
+                            style: TextStyle(
+                              color: Color(0xFFF6F2EC),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            size: 16,
+                            color: Color(0xFFF6F2EC),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                // child: Image(
-                //   image: AssetImage('images/sunset.png'),
-                // ),
               )
             ],
           ),
